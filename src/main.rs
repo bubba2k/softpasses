6459c99bb3d2026114d24e7b7fb114e7cd0103bf
Hans <hschreiter@protonmail.com>
2025-09-03 07:24:36 +0200

    Tidy up asset collection

--- src/main.rs
+++ src/main.rs
@@ -62,12 +62,12 @@ fn main() -> Result<(), std::io::Error> {
     let _sphere4: Hittable = Sphere::new(vec3(-1.1, 0.501, 0.0), 0.45, mat_inner);
     let _sphere2: Hittable = Sphere::new(vec3(0.0, 0.5, 0.0), 0.5, mat_rough.clone());
     let _sphere3: Hittable = Sphere::new(vec3(1.1, 0.5, 0.0), 0.5, mat_metal.clone());
-    let sponza = BVHMesh::from_obj_file(Path::new("assets/bunny_normalized.obj"), mat_rough.clone());
+    let sponza = BVHMesh::from_obj_file(Path::new("assets/models/bunny.obj"), mat_rough.clone());
 
     let objects = vec![sponza, floor];
 
     let env_texture =
-        Texture::from_path(Path::new("assets/Outside1_4K-TONEMAPPED.jpg")).unwrap();
+        Texture::from_path(Path::new("assets/hdri/brownstudio_4k.hdr")).unwrap();
 
     let background = Background::from_environment_texture(env_texture, 2.8);
 
