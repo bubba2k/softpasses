d65f474dfd0b5af4c37902839ddf955b72b42ab5
Hans <hschreiter@protonmail.com>
2025-10-03 10:52:38 +0200

    Implement BVHDebug pipeline

--- src/tracer/hittable.rs
+++ src/tracer/hittable.rs
@@ -8,6 +8,8 @@ use std::path::Path;
 
 pub struct RayInfo {
     pub num_aabb_intersects: u32,
+    pub num_aabb_checks: u32,
+    pub num_primitive_checks: u32,
     pub num_bounces: u32,
 }
 
@@ -19,6 +21,8 @@ pub struct HitRecord {
     pub t: Float,
     pub front_face: bool, // True if ray hit the front of a face/surface. False if ray is on inside
     pub num_aabb_intersects: u32, // For BVH debugging
+    pub num_aabb_checks: u32,
+    pub num_primitive_checks: u32,
 }
 
 impl<'a> HitRecord {
@@ -30,6 +34,8 @@ impl<'a> HitRecord {
         obj_mat: &'a Material,
         obj_normal: Vec3f,
         num_aabb_intersects: u32,
+        num_aabb_checks: u32,
+        num_primitve_checks: u32,
     ) -> Self {
         // Check whether we hit the inside or outside
         if ray.dir.dot(obj_normal) > 0.0 {
@@ -42,6 +48,8 @@ impl<'a> HitRecord {
                 t: t_hit,
                 front_face: false,
                 num_aabb_intersects: num_aabb_intersects,
+                num_aabb_checks: num_aabb_checks,
+                num_primitive_checks: num_primitve_checks,
             }
         } else {
             // Ray hit the face
@@ -53,6 +61,8 @@ impl<'a> HitRecord {
                 t: t_hit,
                 front_face: true,
                 num_aabb_intersects: num_aabb_intersects,
+                num_aabb_checks: num_aabb_checks,
+                num_primitive_checks: num_primitve_checks,
             }
         }
     }
@@ -344,6 +354,8 @@ impl HittableTrait for Sphere {
                     &self.material,
                     sphere_normal,
                     ray_info.num_aabb_intersects,
+                    ray_info.num_aabb_checks,
+                    ray_info.num_primitive_checks,
                 ))
             } else {
                 None
@@ -437,6 +449,8 @@ impl HittableTrait for Plane {
                 &self.material,
                 self.normal,
                 ray_info.num_aabb_intersects,
+                ray_info.num_aabb_checks,
+                ray_info.num_primitive_checks,
             ))
         }
     }
@@ -642,6 +656,8 @@ impl HittableTrait for Parallelogram {
                     &self.material,
                     self.normal,
                     ray_info.num_aabb_intersects,
+                    ray_info.num_aabb_checks,
+                    ray_info.num_primitive_checks,
                 ))
             } else {
                 None
@@ -1072,6 +1088,8 @@ impl HittableTrait for Mesh {
                     ray_info.num_bounces,
                     &self.material,
                     obj_normal,
+                    ray_info.num_aabb_intersects,
+                    ray_info.num_aabb_checks,
                     ray_info.num_aabb_intersects,
                 ))
             } else {
