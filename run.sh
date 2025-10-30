16f4bd4443b98b9770a8737f9a8f1ab9d9e41b08
Hans <hschreiter@protonmail.com>
2025-10-30 02:55:34 +0100

    Update run script

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
+# This simply recompiles (if necessary), runs the program and then opens the output files in GIMP.
+# Useful for dev cycle
+
 if [[ $# -ne 1 ]]
 then
 	printf "Please supply an output path\n"
@@ -5,4 +8,6 @@ then
 fi
 
 cargo run --release $1
-gimp ${1}/* & 2> /dev/null
+gimp ${1}/*.png & 2> /dev/null
+gimp ${1}/*.exr & 2> /dev/null
+
